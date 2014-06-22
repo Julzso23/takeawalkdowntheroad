7e392c9e3835b7b1113f18f7adf0cbb57017b67e
Julzso23 <julian@julxzs.eu>
2014-06-22 17:07:06 +0100

    Added basic mod functionality

---
+++ lua/userContent/mods.lua
@@ -0,0 +1,19 @@
+function addModFolder( path )
+	if not love.filesystem.exists( path ) then
+		love.filesystem.createDirectory( path )
+	end
+
+	for k, v in pairs( love.filesystem.getDirectoryItems( path ) ) do
+		if love.filesystem.isFile( path.."/"..v ) then
+			if string.find( path.."/"..v, ".lua" ) then
+				love.filesystem.load( path.."/"..v )()
+			end
+		elseif love.filesystem.isDirectory( path.."/"..v ) then
+			addModFolder( path.."/"..v )
+		end
+	end
+end
+
+hook.add( "load", "loadMods", function ()
+	addModFolder( "mods" )
+end )
