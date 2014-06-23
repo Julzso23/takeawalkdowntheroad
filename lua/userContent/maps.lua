81aa6a2a901b1f1b1f4a784df94e63ed8cdaf74c
Julzso23 <julian@julxzs.eu>
2014-06-23 16:30:19 +0100

    Added loading maps from appdata

---
+++ lua/userContent/maps.lua
@@ -0,0 +1,40 @@
+maps = {}
+maps.mapList = {}
+
+function maps.loadFiles(folder)
+	local msg = require("lib/MessagePack")
+
+	if not love.filesystem.exists(path) then
+		love.filesystem.createDirectory(path)
+	end
+
+	for k, v in pairs(love.filesystem.getDirectoryItems(path)) do
+		if love.filesystem.isFile(path.."/"..v) then
+			if string.find(path.."/"..v, ".lua") then
+				maps.mapList[v:gsub(".lua", "")] = msg.unpack(love.filesystem.read(path.."/"..v))
+			end
+		elseif love.filesystem.isDirectory(path.."/"..v) then
+			maps.loadFiles(path.."/"..v)
+		end
+	end
+end
+
+function maps.load(map, world)
+	if maps.mapList[map] then
+		for k, v in pairs(maps.mapList[map]) do
+			if not v.x then
+				print("Failed to load tile from map: " .. map .. ". Missing x position value")
+			elseif not v.y then
+				print("Failed to load tile from map: " .. map .. ". Missing y position value")
+			elseif not v.w then
+				print("Failed to load tile from map: " .. map .. ". Missing width value")
+			elseif not v.h then
+				print("Failed to load tile from map: " .. map .. ". Missing height value")
+			end
+
+			tile.create(world, v.x, v.y, v.w, v.h)
+		end
+	else
+		print("Failed to load map: " .. map .. ". Missing file.")
+	end
+end
